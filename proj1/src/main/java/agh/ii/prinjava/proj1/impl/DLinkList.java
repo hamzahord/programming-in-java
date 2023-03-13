202046cca3fe3020c4c7e14d261473c560228b63
hamza <hamza.hord@gmail.com>
2023-03-13 23:14:46 +0100

    project push 2 premières fonctions DlinkedList

--- proj1/src/main/java/agh/ii/prinjava/proj1/impl/DLinkList.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/impl/DLinkList.java
@@ -6,14 +6,40 @@ public class DLinkList<E> {
         T elem;
         Node<T> next;
         Node<T> prev;
+        Node(T elem){
+            this.elem = elem;
+            this.next = null;
+            this.prev = null;
+        }
     }
 
     public void addFirst(E x) {
-        //TODO
+        Node<E> n_node = new Node(x);
+        n_node.next = null;
+        //if list is empty
+        if (this.head == null) {
+            this.head = n_node;
+        }
+        else {
+            n_node.next = this.head;
+            this.head = n_node;
+        }
     }
 
     public void addLast(E x) {
-        //TODO
+        Node<E> n_node = new Node(x);
+        n_node.next = null;
+        //if list is empty
+        if (this.head == null) {
+            this.head = n_node;
+        }
+        else {
+            Node last = this.head;
+            while (last.next != null){
+                last = last.next;
+            }
+            last.next = n_node;
+        }
     }
 
     public E removeFirst(){
@@ -30,4 +56,6 @@ public class DLinkList<E> {
         //TODO
         throw new IllegalStateException("to be done with");
     }
+
+    Node head; //head of the list
 }
